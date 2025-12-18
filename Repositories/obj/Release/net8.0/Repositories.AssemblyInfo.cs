aa912bdb91d9f85a6840eaffbf7cdaa46968df2e
canersinan00 <canersinan1013@gmail.com>
2025-12-18 16:24:46 +0300

    Book Repository Created

--- Repositories/obj/Release/net8.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Release/net8.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c02c15262442a42a748dc3c89414ff57792b8ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+468b398cc1f672d31dc651993e6164ae12011143")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
