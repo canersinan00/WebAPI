cbbeda8d47220a1d1b97cf1a7cda6319fc63864f
canersinan00 <canersinan1013@gmail.com>
2025-12-23 12:13:01 +0300

    Async Code Update For Repository Layer

--- Repositories/Contracts/IBookRepository.cs
+++ Repositories/Contracts/IBookRepository.cs
@@ -1,4 +1,4 @@
-﻿using Entities.Models;
+using Entities.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,8 +9,8 @@ namespace Repositories.Contracts
 {
     public interface IBookRepository : IRepositoryBase<Book>
     {
-        IQueryable<Book> GetAllBooks(bool trackChanges);
-        Book GetOneBookById(int id, bool trackChanges);
+        Task<IEnumerable<Book>> GetAllBooksAsync(bool trackChanges);
+        Task<Book> GetOneBookByIdAsync(int id, bool trackChanges);
         void CreateOneBook(Book book);
         void UpdateOneBook(Book book);
         void DeleteOneBook(Book book);
--- Repositories/Contracts/IRepositoryManager.cs
+++ Repositories/Contracts/IRepositoryManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +9,6 @@ namespace Repositories.Contracts
     public interface IRepositoryManager
     {
         IBookRepository Book {  get; }
-        void Save();
+        Task SaveAsync();
     }
 }
