23bc717fdc2130af3969ce68611295e495957076
canersinan00 <canersinan1013@gmail.com>
2025-12-30 11:55:33 +0300

    BooksController Update for Presentation Layer

--- Presentation/Controller/BooksController.cs
+++ Presentation/Controller/BooksController.cs
@@ -1,6 +1,7 @@
 using Entities.DataTransferObject;
 using Entities.Exceptions;
 using Entities.Models;
+using Entities.RequestFeatures;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Presentation.ActionFilter;
@@ -26,9 +27,9 @@ namespace Presentation.Controller
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAllBooksAsync()
+        public async Task<IActionResult> GetAllBooksAsync([FromQuery] BookParameters bookParameters)
         {
-            var books = await _manager.BookService.GetAllBooksAsync(false);
+            var books = await _manager.BookService.GetAllBooksAsync(bookParameters,false);
             return Ok(books);
         }
 
