46159c52f32c93d1f4e4b5eb87bfb2753854e1c0
canersinan00 <canersinan1013@gmail.com>
2025-12-30 11:38:39 +0300

    RequestParameters Created for Pagination

--- Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d017878197fe7326a29fbc1048d5d6e91452d4f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbe8648b94d0cf2357bc550ce560544a1760497b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
