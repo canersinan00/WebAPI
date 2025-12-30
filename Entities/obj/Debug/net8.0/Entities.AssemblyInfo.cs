aae4ff9fd0107c16807a48e639a1126009dc2674
canersinan00 <canersinan1013@gmail.com>
2025-12-30 15:47:09 +0300

    PagedList Created

--- Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
+++ Entities/obj/Debug/net8.0/Entities.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23bc717fdc2130af3969ce68611295e495957076")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a67ee45cb11bff7839974627ee455ea1532cf525")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
