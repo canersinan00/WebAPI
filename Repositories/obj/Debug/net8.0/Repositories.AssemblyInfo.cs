93821064ed15880e8b23c1f66c5453ee3e93f06e
canersinan00 <canersinan1013@gmail.com>
2026-01-08 14:13:44 +0300

    OrderQueryBuilder Crated and Used

--- Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c531989067046d2505dbf45a2bbcd1675f3f76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dc5f8f5d8c70a1e788ce3be87f27b12c1b29d72")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c531989067046d2505dbf45a2bbcd1675f3f76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dc5f8f5d8c70a1e788ce3be87f27b12c1b29d72")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
