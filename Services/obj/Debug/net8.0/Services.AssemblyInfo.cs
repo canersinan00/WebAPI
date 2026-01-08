24c3e7be666c4b2df432a70a7171597c9e30481e
canersinan00 <canersinan1013@gmail.com>
2026-01-08 15:00:55 +0300

    IDataShaped ınterface Created

--- Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dc5f8f5d8c70a1e788ce3be87f27b12c1b29d72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93821064ed15880e8b23c1f66c5453ee3e93f06e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
